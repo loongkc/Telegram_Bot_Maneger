b9fd3b8c24737828864be3a63bb0c6980606c993
loong <1224092499@qq.com>
2024-11-19 21:25:05 +0800

    Update TGBotManager.py
    深浅跟随系统

--- TGBotManager.py
+++ TGBotManager.py
@@ -327,7 +327,7 @@ class MainWindow(ctk.CTk):
 
 
 if __name__ == "__main__":
-    ctk.set_appearance_mode("dark")
+    ctk.set_appearance_mode("system")
     ctk.set_default_color_theme("blue")
     app = MainWindow()
     app.mainloop()
